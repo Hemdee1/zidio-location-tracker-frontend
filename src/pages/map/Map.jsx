871c731c189ba60e31a63c5d3897104c5358a633
unknown <jefferyamon2468@gmail.com>
2024-03-26 16:16:17 +0000

    added login,sign-up and map to repo

---
+++ src/pages/map/Map.jsx
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect, useState } from "react";
+import * as maptilersdk from "@maptiler/sdk";
+import "@maptiler/sdk/dist/maptiler-sdk.css";
+import "./map.css";
+
+const Map = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const accra = { lng: -0.22571, lat: 5.56454 };
+  const [zoom] = useState(12);
+  maptilersdk.config.apiKey = "1RziHFb3U1x5VohitdkH";
+
+  useEffect(() => {
+    if (map.current) return; // stops map from intializing more than once
+
+    map.current = new maptilersdk.Map({
+      container: mapContainer.current,
+      style: maptilersdk.MapStyle.STREETS,
+      center: [accra.lng, accra.lat],
+      zoom: zoom,
+    });
+
+    new maptilersdk.Marker({ color: "#FF0000" })
+      .setLngLat([-0.192897, 5.549032])
+      .addTo(map.current);
+  }, [accra.lng, accra.lat, zoom]);
+
+  return (
+    <div className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
+};
+export default Map;
---
+++ src/pages/map/MapWrapper.jsx
@@ -0,0 +1,13 @@
+import Map from "./Map";
+import Navbar from "./Navbar";
+
+const MapWrapper = () => {
+  return (
+    <>
+      <Navbar />
+      <Map />
+    </>
+  );
+};
+
+export default MapWrapper;
